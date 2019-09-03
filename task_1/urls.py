07c3012fa854048f51e4ac611e87a273114e540b
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-09-03 10:05:16 +0300

    Mohammad Al Thunayan

--- task_1/urls.py
+++ task_1/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('flights/', views.FlightsList.as_view(), name="flights-list"),
     path('bookings/', views.BookingsList.as_view(), name="bookings-list"), 
+    path('bookings/detail/<int:booking_id>/', views.BookingDetail.as_view(), name="booking-details"), 
+    path('bookings/update/<int:booking_id>/', views.BookingUpdate.as_view(), name="update-booking"), 
+    path('bookings/delete/<int:booking_id>/', views.BookingDelete.as_view(), name="cancel-booking"), 
 ]
